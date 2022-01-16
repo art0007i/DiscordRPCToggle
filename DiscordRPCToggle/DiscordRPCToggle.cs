b0eb5d800daad4214bd51b0db0557d79c70962fc
art0007i <art0007i@gmail.com>
2022-01-16 01:33:01 +0100

    Add project files.

---
+++ DiscordRPCToggle/DiscordRPCToggle.cs
@@ -0,0 +1,56 @@
+using HarmonyLib;
+using NeosModLoader;
+using System;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using FrooxEngine;
+using FrooxEngine.UIX;
+using BaseX;
+
+namespace DiscordRPCToggle
+{
+    public class DiscordRPCToggle : NeosMod
+    {
+        public override string Name => "DiscordRPCToggle";
+        public override string Author => "art0007i";
+        public override string Version => "1.0.0";
+        public override string Link => "https://github.com/art0007i/DiscordRPCToggle/";
+
+        private static readonly ModConfigurationKey<bool> KEY_RPC = new ModConfigurationKey<bool>("rpc_enable", "If false discord rpc will be disabled", () => true);
+
+        public override ModConfigurationDefinition GetConfigurationDefinition()
+        {
+            List<ModConfigurationKey> keys = new List<ModConfigurationKey>();
+            keys.Add(KEY_RPC);
+            return DefineConfiguration(new Version(1, 0, 0), keys);
+        }
+
+        private static ModConfiguration config;
+
+        public override void OnEngineInit()
+        {
+            config = GetConfiguration();
+            Harmony harmony = new Harmony("me.art0007i.DiscordRPCToggle");
+            harmony.PatchAll();
+        }
+
+        [HarmonyPatch(typeof(DiscordPlatformConnector))]
+        class DiscordPlatformConnector_Patch
+        {
+            [HarmonyPrefix]
+            [HarmonyPatch("SetCurrentStatus")]
+            public static bool Prefix()
+            {
+                bool yes = config.GetValue(KEY_RPC);
+                if (!yes)
+                {
+                    var discord = Engine.Current.PlatformInterface.GetConnectors<DiscordPlatformConnector>().First();
+                    discord.ClearCurrentStatus();
+                }
+                return yes;
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+# DiscordRPCToggle
+
+A [NeosModLoader](https://github.com/zkxs/NeosModLoader) mod for [Neos VR](https://neos.com/) that allows you to toggle discord rich presence for neos with a config option.
+
+## Installation
+1. Install [NeosModLoader](https://github.com/zkxs/NeosModLoader).
+1. Place [DiscordRPCToggle.dll](https://github.com/art0007i/DiscordRPCToggle/releases/latest/download/DiscordRPCToggle.dll) into your `nml_mods` folder. This folder should be at `C:\Program Files (x86)\Steam\steamapps\common\NeosVR\nml_mods` for a default install. You can create it if it's missing, or if you launch the game once with NeosModLoader installed it will create the folder for you.
+1. Start the game. If you want to verify that the mod is working you can check your Neos logs.
